f35a57198619575a2d89bd0640ba3cc45a0fc049
Luca <51205600+Gi0laby@users.noreply.github.com>
2026-03-26 22:59:55 +0800

    Update WARP-info.js

--- WARP-info.js
+++ WARP-info.js
@@ -24,10 +24,9 @@ $httpClient.get("https://cloudflare.com/cdn-cgi/trace", function(error, response
         const ipv4 = info.ip || "未知";
         const loc = info.loc || "??";
         const colo = info.colo || "未知";
-        const warpStatus = (info.warp === "on" || info.warp === "plus") ? "PLUS | 增強" : "未啟用";
+        const warpStatus = info.warp || "off";   // 直接顯示 on / plus / off
 
         const content = `IPv4: ${ipv4}\n` +
-                        `IPv6: 查詢中...` +
                         `\n託管中心: ${loc} | ${colo}` +
                         `\n隱私保護: ${warpStatus}`;
 
